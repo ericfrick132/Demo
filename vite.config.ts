fd959e48db5e66d03bcb828b33e30f2aa88ffcef
Eric F <eric.frick@efcloud.tech>
2025-09-03 08:46:55 -0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Demo/',
   server: {
     port: 3001,
     host: '0.0.0.0'  // 👈 Agrega esta línea
